9298e80bf8f3fa6c4e37dc30425896a0737c6063
Jamilur Rahman <r.jamilur96@gmail.com>
2022-06-18 03:27:54 +0600

    routes setup complete w/ componenets and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Create from "./pages/create/Create";
+import Catalogue from "./pages/catalogue/Catalogue";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
-    <div className="App">
-      Hi Mom
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          {/* Nested Route for Users */}
+          <Route path="users">
+            <Route index element={<Catalogue />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="create" element={<Create />} />
+          </Route>
+          {/* Nested Route for Products */}
+          <Route path="products">
+            <Route index element={<Catalogue />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="create" element={<Create />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <p>this is the home page</p>
+    </div>
+  );
+}
+
+export default Home;
